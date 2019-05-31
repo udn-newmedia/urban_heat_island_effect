4f19944c41303ecf0988d556c62cc106d882ddef
Jim <wowdacom@gmail.com>
2019-05-31 18:24:08 +0800

    update

--- src/components/chart2.vue
+++ src/components/chart2.vue
@@ -4,7 +4,7 @@
       <section  id="chart2-background">
         <mq-layout :mq="['xs', 's', 'm']">
           <svg class="chart2-mob" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-	 viewBox="0 0 520 925" style="enable-background:new 0 0 520 925;" xml:space="preserve">
+              viewBox="0 0 520 925" style="enable-background:new 0 0 520 925;" xml:space="preserve">
             <g id="heat_x5F_island_x5F_chart1_x5F_background_x5F_mob">
               <rect x="432.23" y="170.25" class="st0" width="13.88" height="13"/>
               <rect x="344.43" y="170.25" class="st1" width="13.88" height="13"/>
@@ -72,7 +72,7 @@
               </g>
             </g>
             <g id="heat_x5F_island_x5F_chart1_x5F_taipei_x5F_mob">
-              <polyline :class="lineOne" class="st8" points="54.82,506.11 60.74,474.44 66.66,533.7 72.16,457.07 78.08,533.7 84,485.68 89.5,484.65 
+              <polyline class="st8" points="54.82,506.11 60.74,474.44 66.66,533.7 72.16,457.07 78.08,533.7 84,485.68 89.5,484.65 
                 95.42,536.76 106.84,473.42 113.18,467.29 118.68,442.76 124.17,535.74 130.09,509.18 135.59,512.24 141.51,481.59 147.43,462.18 
                 153.35,473.42 158.85,459.11 164.77,515.31 170.69,513.26 176.19,458.09 182.11,487.72 188.03,443.79 193.53,498.96 199.02,532.68 
                 204.94,515.31 210.87,501 216.36,499.98 222.28,503.05 228.2,463.2 233.7,491.81 239.62,482.61 245.54,499.98 251.04,428.46 
@@ -83,7 +83,7 @@
                 470.52,343.66 476.01,325.26 481.93,333.44 487.85,331.4 	"/>
             </g>
             <g id="heat_x5F_island_x5F_chart1_x5F_tamsui_x5F_mob">
-              <polyline :class="lineTwo" class="st9" points="55.24,501 61.16,505.09 66.24,536.76 72.16,450.94 78.08,525.52 84,475.46 89.5,474.44 95.42,522.46 
+              <polyline class="st9" points="55.24,501 61.16,505.09 66.24,536.76 72.16,450.94 78.08,525.52 84,475.46 89.5,474.44 95.42,522.46 
                 106.84,467.29 112.76,472.39 118.25,443.79 124.17,520.42 130.09,499.98 135.59,503.05 141.51,478.52 147.43,460.13 152.93,459.11 
                 158.43,446.85 164.77,515.31 170.27,509.18 176.19,474.44 181.69,491.81 187.18,466.26 193.53,510.2 199.45,539.83 204.94,534.72 
                 210.87,513.26 216.36,507.13 222.28,515.31 228.2,480.57 234.12,509.18 239.62,504.07 245.54,523.48 251.04,445.83 256.96,495.89 
@@ -93,7 +93,7 @@
                 418.5,461.15 424.42,465.24 429.92,481.59 435.84,449.92 441.34,485.68 447.26,541.87 453.18,504.07 458.67,446.85 464.59,439.7 
                 470.09,410.07 476.01,421.31 481.93,401.89 487.85,413.13 	"/>
             </g>
-            <g :class="{'current_group': steps[1] }" class="first_group" id="heat_x5F_island_x5F_chart1_x5F_1985_x5F_mob">
+            <g id="heat_x5F_island_x5F_chart1_x5F_1985_x5F_mob">
               <circle class="st1" cx="297.65" cy="453.49" r="4.83"/>
               <circle class="st0" cx="297.65" cy="514.21" r="4.83"/>
               <g>
@@ -108,12 +108,12 @@
                 <g>
                   <line class="st12" x1="296.92" y1="227.18" x2="296.92" y2="229.68"/>
                   <line class="st13" x1="296.92" y1="232.72" x2="296.92" y2="247.41"/>
-                  <line class="st14 line_move" x1="296.92" y1="248.93" x2="296.92" y2="709.5"/>
+                  <line class="st14" x1="296.92" y1="248.93" x2="296.92" y2="709.5"/>
                   <line class="st12" x1="296.92" y1="711.02" x2="296.92" y2="713.52"/>
                 </g>
               </g>
             </g>
-            <g :class="groupTwo" class="sec_group" id="heat_x5F_island_x5F_chart1_x5F_2018_x5F_mob">
+            <g id="heat_x5F_island_x5F_chart1_x5F_2018_x5F_mob">
               <g>
                 <text transform="matrix(1.1018 0 0 1 460.6094 318.2119)" class="st1 st10 st7">23.9</text>
               </g>
@@ -134,7 +134,7 @@
                 </g>
               </g>
             </g>
-            <g :class="{'current_group': steps[3] }" class="third_group" id="heat_x5F_island_x5F_chart1_x5F_1980_x5F_mob">
+            <g id="heat_x5F_island_x5F_chart1_x5F_1980_x5F_mob">
               <g>
                 <text transform="matrix(1.1018 0 0 1 239.1152 547.6455)" class="st1 st10 st11">22.5</text>
               </g>
@@ -142,7 +142,7 @@
               <circle class="st0" cx="260.92" cy="493.83" r="4.83"/>
               <text transform="matrix(1.046 0 0 1 240.2539 420.4443)" class="st0 st10 st11">22.6</text>
             </g>
-            <g :class="{'current_group': steps[4] }" class="forth_group" id="heat_x5F_island_x5F_chart1_x5F_1986_x5F_mob">
+            <g id="heat_x5F_island_x5F_chart1_x5F_1986_x5F_mob">
               <g>
                 <text transform="matrix(1.1018 0 0 1 252.8945 431.4736)" class="st1 st10 st11">22.4</text>
               </g>
@@ -150,7 +150,7 @@
               <circle class="st0" cx="302.84" cy="553.66" r="4.83"/>
               <text transform="matrix(1.046 0 0 1 250.1719 558.2734)" class="st0 st10 st11">21.6</text>
             </g>
-            <g :class="groupFive" class="five_group" id="heat_x5F_island_x5F_chart1_x5F_1987_x5F_mob">
+            <g id="heat_x5F_island_x5F_chart1_x5F_1987_x5F_mob">
               <g>
                 <text transform="matrix(1.1018 0 0 1 269.1221 396.4561)" class="st1 st10 st11">23.0</text>
               </g>
@@ -158,11 +158,11 @@
               <circle class="st0" cx="309.06" cy="522.64" r="4.83"/>
               <text transform="matrix(1.046 0 0 1 271.3994 586.2559)" class="st0 st10 st11">21.9</text>
             </g>
-          </svg>
+            </svg>
         </mq-layout>
         <mq-layout :mq="['l', 'xl']">
           <svg class="chart2-web" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-	 viewBox="0 0 1280 720" style="enable-background:new 0 0 1280 720;" xml:space="preserve">
+              viewBox="0 0 1280 720" style="enable-background:new 0 0 1280 720;" xml:space="preserve">
             <g id="heat_x5F_island_x5F_chart1_x5F_background">
               <line class="st0" x1="155.77" y1="600.36" x2="1144.76" y2="600.36"/>
               <line class="st0" x1="168.79" y1="600" x2="168.79" y2="611.34"/>
@@ -237,7 +237,7 @@
               </g>
             </g>
             <g id="heat_x5F_island_x5F_chart1_x5F_taipei">
-              <polyline :class="lineOneWeb" class="st9" points="172.08,408.5 185.38,377.5 198.67,435.5 211.01,360.5 224.3,435.5 237.59,388.5 249.94,387.5 
+              <polyline class="st9" points="172.08,408.5 185.38,377.5 198.67,435.5 211.01,360.5 224.3,435.5 237.59,388.5 249.94,387.5 
                 263.23,438.5 288.86,376.5 303.1,370.5 315.45,346.5 327.79,437.5 341.08,411.5 353.42,414.5 366.71,384.5 380.01,365.5 
                 393.3,376.5 405.64,362.5 418.93,417.5 432.22,415.5 444.57,361.5 457.86,390.5 471.15,347.5 483.49,401.5 495.84,434.5 
                 509.13,417.5 522.42,403.5 534.76,402.5 548.05,405.5 561.35,366.5 573.69,394.5 586.98,385.5 600.27,402.5 612.61,332.5 
@@ -248,7 +248,7 @@
                 1105.36,249.5 1117.7,231.5 1131,239.5 1144.29,237.5 	"/>
             </g>
             <g id="heat_x5F_island_x5F_chart1_x5F_tamsui">
-              <polyline :class="lineTwoWeb" class="st10" points="173.03,403.5 186.32,407.5 197.72,438.5 211.01,354.5 224.3,427.5 237.59,378.5 249.94,377.5 
+              <polyline class="st10" points="173.03,403.5 186.32,407.5 197.72,438.5 211.01,354.5 224.3,427.5 237.59,378.5 249.94,377.5 
                 263.23,424.5 288.86,370.5 302.15,375.5 314.5,347.5 327.79,422.5 341.08,402.5 353.42,405.5 366.71,381.5 380.01,363.5 
                 392.35,362.5 404.69,350.5 418.93,417.5 431.27,411.5 444.57,377.5 456.91,394.5 469.25,369.5 483.49,412.5 496.78,441.5 
                 509.13,436.5 522.42,415.5 534.76,409.5 548.05,417.5 561.35,383.5 574.64,411.5 586.98,406.5 600.27,425.5 612.61,349.5 
@@ -258,7 +258,7 @@
                 975.29,417.5 988.58,364.5 1001.88,368.5 1014.22,384.5 1027.51,353.5 1039.85,388.5 1053.14,443.5 1066.44,406.5 1078.78,350.5 
                 1092.07,343.5 1104.41,314.5 1117.7,325.5 1131,306.5 1144.29,317.5 	"/>
             </g>
-            <g  :class="{'current_group': steps[1] }" class="first_group" id="heat_x5F_island_x5F_chart1_x5F_1985">
+            <g id="heat_x5F_island_x5F_chart1_x5F_1985">
               <g>
                 <text transform="matrix(1.1018 0 0 1 668.9922 454.4883)" class="st1 st11 st12">22.0</text>
               </g>
@@ -268,7 +268,7 @@
               </g>
               <circle class="st4" cx="717.1" cy="359.5" r="6"/>
             </g>
-            <g class="first_group" id="heat_x5F_island_x5F_chart1_x5F_1985line">
+            <g id="heat_x5F_island_x5F_chart1_x5F_1985line">
               <g>
                 <g>
                   <line class="st13" x1="715.61" y1="135.5" x2="715.61" y2="138"/>
@@ -278,7 +278,7 @@
                 </g>
               </g>
             </g>
-            <g :class="groupTwo" class="sec_group" id="heat_x5F_island_x5F_chart1_x5F_2018">
+            <g id="heat_x5F_island_x5F_chart1_x5F_2018">
               <circle class="st1" cx="1144.76" cy="318" r="6"/>
               <text transform="matrix(1.046 0 0 1 1152.1611 321.9883)" class="st1 st11 st12">23.0</text>
               <circle class="st4" cx="1143.81" cy="237" r="6"/>
@@ -292,7 +292,7 @@
             <g id="heat_x5F_island_x5F_chart1_x5F_line">
               <line class="st16" x1="650.88" y1="135.5" x2="650.88" y2="611.34"/>
             </g>
-            <g :class="{'current_group': steps[3] }" class="third_group" id="heat_x5F_island_x5F_chart1_x5F_1980">
+            <g id="heat_x5F_island_x5F_chart1_x5F_1980">
               <g>
                 <text transform="matrix(1.1018 0 0 1 646.2217 408.9883)" class="st4 st11 st12">22.5</text>
               </g>
@@ -300,7 +300,7 @@
               <circle class="st4" cx="653" cy="375" r="6"/>
               <circle class="st1" cx="653" cy="360" r="6"/>
             </g>
-            <g  :class="{'current_group': steps[4] }" class="forth_group" id="heat_x5F_island_x5F_chart1_x5F_1986">
+            <g id="heat_x5F_island_x5F_chart1_x5F_1986">
               <g>
                 <text transform="matrix(1.1018 0 0 1 739.1611 384.9883)" class="st4 st11 st12">22.4</text>
               </g>
@@ -308,7 +308,7 @@
               <circle class="st4" cx="729.81" cy="379" r="6"/>
               <circle class="st1" cx="728.76" cy="457" r="6"/>
             </g>
-            <g  :class="groupFive" class="five_group" id="heat_x5F_island_x5F_chart1_x5F_1987">
+            <g id="heat_x5F_island_x5F_chart1_x5F_1987">
               <g>
                 <text transform="matrix(1.1018 0 0 1 718.5918 303.9883)" class="st4 st11 st12">23.0</text>
               </g>
@@ -480,7 +480,7 @@ export default {
   #chart2-background {
     .chart2-mob {
       height: 100vh;
-      width: 100vw;
+      width: 100%;
       .st0{fill:#707070;}
       .st1{fill:#EA0303;}
       .st2{fill:#605F5F;}
@@ -489,187 +489,36 @@ export default {
       .st5{fill:none;stroke:#969696;stroke-width:0.25;stroke-miterlimit:10;}
       .st6{font-family:'ArialMT';}
       .st7{font-size:22.08px;}
-      .st8{
-        fill:none;
-        stroke:#EA0303;
-        stroke-width:1.5;
-        stroke-miterlimit:10;
-        stroke-dasharray: 2520;
-        // animation: red 5s linear infinite;
-      }
-      .st9{
-        fill:none;
-        stroke:#707070;
-        stroke-width:1.5;
-        stroke-miterlimit:10;
-        stroke-dasharray: 2520;
-        // animation: gray 5s linear infinite;
-      }
+      .st8{fill:none;stroke:#EA0303;stroke-width:1.5;stroke-miterlimit:10;}
+      .st9{fill:none;stroke:#707070;stroke-width:1.5;stroke-miterlimit:10;}
       .st10{font-family:'Arial-BoldMT';}
       .st11{font-size:21px;}
       .st12{fill:none;stroke:#EA0303;stroke-width:0.25;stroke-miterlimit:10;}
       .st13{fill:none;stroke:#EA0303;stroke-width:0.25;stroke-miterlimit:10;stroke-dasharray:5.0668,3.0401,5.0668,3.0401;}
-      .st14{fill:none;stroke:#EA0303;stroke-width:0.25;stroke-miterlimit:10;stroke-dasharray:5.0668,3.0401,5.0668,3.0401,5.0668,3.0401;}
+      
+        .st14{fill:none;stroke:#EA0303;stroke-width:0.25;stroke-miterlimit:10;stroke-dasharray:5.0668,3.0401,5.0668,3.0401,5.0668,3.0401;}
     }
     .chart2-web {
       height: 100vh;
-      width: 100vw;
+      width: 100%;
       .st0{fill:none;stroke:#969696;stroke-width:0.5;stroke-miterlimit:10;}
       .st1{fill:#707070;}
       .st2{font-family:'ArialMT';}
       .st3{font-size:18px;}
       .st4{fill:#EA0303;}
       .st5{font-family:'ARHeiB5-Light-B5pc-H';}
-      .st6{font-size:17.0895px;glyph-orientation-vertical:0;writing-mode:tb;}
+      .st6{font-size:15.0895px;glyph-orientation-vertical:0;writing-mode:tb;}
       .st7{font-size:17.0895px;}
       .st8{fill:#353535;}
-      .st9{
-        fill:none;
-        stroke:#EA0303;
-        stroke-width:3;
-        stroke-miterlimit:10;
-        stroke-dasharray: 2700;
-        //2700
-      }
-      .st10{
-        fill:none;
-        stroke:#707070;
-        stroke-width:3;
-        stroke-miterlimit:10;
-        stroke-dasharray: 2430;
-        //2430
-      }
+      .st9{fill:none;stroke:#EA0303;stroke-width:3;stroke-miterlimit:10;}
+      .st10{fill:none;stroke:#707070;stroke-width:3;stroke-miterlimit:10;}
       .st11{font-family:'Arial-BoldMT';}
       .st12{font-size:21px;}
       .st13{fill:none;stroke:#EA0303;stroke-width:0.5;stroke-miterlimit:10;}
       .st14{fill:none;stroke:#EA0303;stroke-width:0.5;stroke-miterlimit:10;stroke-dasharray:4.9579,2.9747,4.9579,2.9747;}
-      .st15{fill:none;stroke:#EA0303;stroke-width:0.5;stroke-miterlimit:10;stroke-dasharray:4.9579,2.9747,4.9579,2.9747,4.9579,2.9747;}
+      
+        .st15{fill:none;stroke:#EA0303;stroke-width:0.5;stroke-miterlimit:10;stroke-dasharray:4.9579,2.9747,4.9579,2.9747,4.9579,2.9747;}
       .st16{fill:none;stroke:#EA0303;stroke-width:0.5;stroke-miterlimit:10;stroke-dasharray:5,3,5,3,5,3;}
-    }
-    .line_one_default {
-      stroke-dashoffset: 2520;
-    }
-    .line_one_step1 {
-      animation: redOne 1s linear forwards;
-    }
-    .line_one_step2 {
-      animation: redTwo 1s linear forwards;
-    }
-    .line_one_other_step {
-      stroke-dashoffset: 0;
-    }
-    .line_two_default {
-      stroke-dashoffset: 2520;
-    }
-    .line_two_step1 {
-      animation: grayOne 1s linear forwards;
-    }
-    .line_two_step2 {
-      animation: grayTwo 1s linear forwards;
-    }
-    .line_two_other_step {
-      stroke-dashoffset: 0;
-    }
-    @keyframes redOne {
-      from {
-        stroke-dashoffset: 2520;
-      }
-      to {
-        stroke-dashoffset: 1090;
-      }
-    }
-    @keyframes redTwo {
-      from {
-        stroke-dashoffset: 1015;
-      }
-      to {
-        stroke-dashoffset: 0;
-      }
-    }
-    @keyframes grayOne {
-      from {
-        stroke-dashoffset: 2520;
-      }
-      to {
-        stroke-dashoffset: 1290;
-      }
-    }
-    @keyframes grayTwo {
-      from {
-        stroke-dashoffset: 1250;
-      }
-      to {
-        stroke-dashoffset: 0;
-      }
-    }
-
-    //web
-    .line_one_web_default {
-      stroke-dashoffset: 2700;
-    }
-    .line_one_web_step1 {
-      animation: redOneWeb 1s linear forwards;
-    }
-    .line_one_web_step2 {
-      animation: redTwoWeb 1s linear forwards;
-    }
-    .line_one_web_other_step {
-      stroke-dashoffset: 0;
-    }
-    .line_two_web_default {
-      stroke-dashoffset: 2430;
-    }
-    .line_two_web_step1 {
-      animation: grayOneWeb 1s linear forwards;
-    }
-    .line_two_web_step2 {
-      animation: grayTwoWeb 1s linear forwards;
-    }
-    .line_two_web_other_step {
-      stroke-dashoffset: 0;
-    }
-    @keyframes redOneWeb {
-      from {
-        stroke-dashoffset: 2700;
-      }
-      to {
-        stroke-dashoffset: 1165;
-      }
-    }
-    @keyframes redTwoWeb {
-      from {
-        stroke-dashoffset: 1165;
-      }
-      to {
-        stroke-dashoffset: 0;
-      }
-    }
-    @keyframes grayOneWeb {
-      from {
-        stroke-dashoffset: 2430;
-      }
-      to {
-        stroke-dashoffset: 1080;
-      }
-    }
-    @keyframes grayTwoWeb {
-      from {
-        stroke-dashoffset: 1080;
-      }
-      to {
-        stroke-dashoffset: 0;
-      }
-    }
-    .first_group, .sec_group, .third_group, .forth_group, .five_group {
-      opacity: 0;
-    }
-    .current_group {
-      opacity: 1;
-      transition: all 1s ease-out;
-      transition-delay: 0.5s;
-    }
-    .line_move {
-      transform: translateX(-7%)
     }
   }
   .section {  
